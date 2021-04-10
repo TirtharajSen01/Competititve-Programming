4485fb06e096e17b8c29c3641440c2b4b423f813
TIrtharajSen01 <sentirtharaj01@gmail.com>
2021-04-10 18:47:58 +0530

    Unsolved

--- 1541Leetcode.cpp
+++ 1541Leetcode.cpp
@@ -1,7 +1,32 @@
-#include<bits/stdc++.h>
-using namespace std;
+// When there is the question that check validity or return no of insertions to make valid do this first method
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    
-}
+// int main()
+// {
+//     string s;
+//     cin>>s;
+//     int bal = 0;
+//     int ans = 0;
+
+//     for(int i = 0 ; i < s.length(); i++)
+//     {
+//         bal += s[i] == '(' ? 1 : -1;
+
+//         if(bal == -1)
+//         {
+//             bal += 1;
+//             ans += 1;
+//         }
+//     }
+
+//     int main_answer = bal + ans;
+//     cout<<main_answer<<endl;
+//     return 0;
+// }
+
+
+
+// Now the leetcode question solution 
+
+// left do it later
